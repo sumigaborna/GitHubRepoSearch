8d246aa36e7c9d9d80299dbabe5022a354ee0b11
sumiga.borna <sumiga.borna@gmail.com>
2020-11-15 18:02:14 +0100

    Init recycler adapter

--- app/src/main/java/com/bornasumiga/githubreposearch/repoList/ui/RepoListRecyclerAdapter.kt
+++ app/src/main/java/com/bornasumiga/githubreposearch/repoList/ui/RepoListRecyclerAdapter.kt
@@ -1,3 +1,26 @@
 package com.bornasumiga.githubreposearch.repoList.ui
 
-class RepoListRecyclerAdapter
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RepoListRecyclerAdapter : RecyclerView.Adapter<RepoListViewHolder>(){
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RepoListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class RepoListViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+}
+
